77b152569bd7a4239e072cd32ea03649980a8727
toyman640 <falakoadebayor@gmail.com>
2025-04-19 16:54:49 +0100

    set up indec page

--- skaal_meats/skaal_meats/urls.py
+++ skaal_meats/skaal_meats/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from frontend.views import index_view
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path("", index_view, name='index'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
