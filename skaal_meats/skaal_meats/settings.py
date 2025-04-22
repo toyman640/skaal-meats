2fb578edbd76a0f57117a1e3db55205a6a9e73dc
toyman640 <falakoadebayor@gmail.com>
2025-04-22 13:29:14 +0100

    Fix new section

--- skaal_meats/skaal_meats/settings.py
+++ skaal_meats/skaal_meats/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
+# STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 # Quick-start development settings - unsuitable for production
