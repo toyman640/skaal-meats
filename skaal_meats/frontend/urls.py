eb4ad26bd6191f9b8372bfe7980466c7c5558846
toyman640 <falakoadebayor@gmail.com>
2025-05-17 14:34:22 +0100

    Add about and contact use page

---
+++ skaal_meats/frontend/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from frontend import views
+
+
+app_name= 'frontend'
+
+
+urlpatterns = [
+  path('about-us/', views.about_us_view, name='about_us_view'),
+  path('contact-us/', views.contact_us_view, name='contact_us_view'),
+]
--- skaal_meats/frontend/views.py
+++ skaal_meats/frontend/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index_view(request):
   return render(request, "index.html")
+
+
+
+def about_us_view(request):
+  return render(request, "about.html")
+
+
+def contact_us_view(request):
+  return render(request, "contact.html")
